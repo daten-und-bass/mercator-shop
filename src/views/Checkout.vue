a744733f2de6716104abb290b009ad9d8640b551
arne-at-daten-und-bass <arne@daten-und-bass.io>
2019-03-27 11:41:08 +0100

    Last commit (no way to format email properly)

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -102,6 +102,7 @@
                 <a :href="mailLink">Mail</a>
               </v-flex>
             </v-layout>
+            <pre>{{mailLink}}</pre>
           </v-card-text>
         </v-card>
         <div class="mercator-text--center">
@@ -161,8 +162,50 @@ export default {
       shippingPostal: 'orderTypeOfShippingPostal',
       orderProductsAllPricePostTax: 'orderProductsAllPricePostTax',
     }),
+    personalDataString() {
+      const headerName = this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.contact', 1));
+      const tableDataName = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.name', 1)).padEnd(30)}${this.order.address_orderer.first_name}${this.order.address_orderer.last_name}`;
+      const tableDataCompany = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.company', 1)).padEnd(30)}${this.order.address_orderer.company ? this.order.address_orderer.company : ''}`;
+      const tableDataAddress = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.address', 1)).padEnd(30)}${(this.order.address_orderer.city || this.order.address_orderer.street || this.order.address_orderer.number || this.order.address_orderer.postcode) ? this.order.address_orderer.street + this.order.address_orderer.number + this.order.address_orderer.postcode + this.order.address_orderer.city : ''}`;
+      const tableDataEmail = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('general.email', 1)).padEnd(30)}${this.order.address_orderer.email}`;
+      const tableDataPhone = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('general.phone', 1)).padEnd(30)}${this.order.address_orderer.phone}`;
+
+      // if company
+      // if address ... mind fixed written comma, address e.g. not in info request or pickup
+
+      // Kommentar: ${this.order.comment}
+      // Woher kennen Sie uns: ${this.order.lead}\n`;
+
+      const personalDataString = `\n${headerName}:\n${tableDataName}\n${tableDataCompany}\n${tableDataEmail}\n${tableDataPhone}\n${tableDataAddress}`;
+
+      return personalDataString;
+    },
+    orderDataString() {
+      const headerName = this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.checkout', 1));
+      const orderDate = new Date();
+      const tableDataDate = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc("checkout['order date']", 1)).padEnd(30)}${orderDate.getFullYear()}-${orderDate.getMonth() + 1}-${orderDate.getDate()}`;
+      const tableDataOrderType = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc("checkout['order type']", 1)).padEnd(30)}${this.$i18n.t(`config.${this.orderType}`)}`;
+      const tableDataComment = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('checkout.comment', 1)).padEnd(30)}${this.order.comment ? this.order.comment : ''}`;
+      const tableDataKnowFrom = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.t("checkout['where do you know us from']")).padEnd(30)}${this.order.comment ? this.order.comment : ''}`;
+
+      return `\n\n${headerName}:\n${tableDataOrderType}\n${tableDataDate}\n${tableDataComment}\n${tableDataKnowFrom}`;
+    },
+    productDataString() {
+      const headerProducts = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('products.product', 2))}`;
+      const headerProduct = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('products.product', 1)).padEnd(30)}`;
+      const headerPrice = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('products.price', 1)).padEnd(10)}`;
+      const headerAmount = `${this.$options.filters.capitalizeFirstLetter(this.$i18n.tc('products.amount', 1)).padEnd(10)}`;
+
+      // Datum: ${new Date()}
+      // Bestelltyp: ${this.orderType} \n
+      let productDataString = `\n\n${headerProducts}:\n${headerProduct}${headerPrice}${headerAmount}\n`;
+      this.orderProductsAll.forEach((e) => {
+        productDataString += `${e.title.de.padEnd(30)}${this.$options.filters.showAsCurrency(e.price).padEnd(10)}${e.quantity.toString().padEnd(10)}\n`;
+      });
+      return productDataString;
+    },
     mailLink() {
-      const recipient = `${this.shop['E-Mail']}`;
+      const recipient = `${this.shop.email}`;
       const subject = encodeURIComponent(`Mercator Shop Anfrage von ${this.order.address_orderer.first_name} ${this.order.address_orderer.last_name}`);
       let body = '';
 
@@ -171,7 +214,8 @@ export default {
           body = encodeURIComponent(this.orderType);
           break;
         case 'pickup':
-          body = encodeURIComponent(this.orderType);
+          // body = encodeURIComponent(this.orderType);
+          body = `${this.personalDataString}${this.orderDataString}${this.productDataString}`;
           break;
         case 'shipping_postal':
           body = encodeURIComponent(this.orderType);
@@ -181,6 +225,7 @@ export default {
       }
 
       return `mailto:${recipient}?subject=${subject}&body=${body}`;
+      // return `${recipient}\n${subject}\n${body}`;
     },
   },
 };
